abaa7d2e27492ebfe1c0dc6d369a28f52323baeb
Marcos8060 <marcosgav80@gmail.com>
2022-08-15 14:15:49 +0300

    mocked up the landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import image from '../images/farm.jpg'
+import styled from 'styled-components'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <HeroWrapper>
+        <div className='pt-32 text-center w-10/12 mx-auto'>
+            <div>
+                <p className='text-white uppercase underline mb-10 font-semibold font-press-start'>Welcome to our online survey</p>
+                <h1 className='text-white md:text-7xl text-4xl font-semibold font-press-start'>Agriculture <br /> Organic Products</h1>
+                <div className="mt-10">
+                    <button className='bg-brown p-4 rounded m-2 uppercase font-semibold text-green font-press-start'>Take survey</button>
+                    <button className='bg-brown p-4 rounded m-2 uppercase font-semibold text-green font-press-start'>Responses</button>
+                </div>
+            </div>
+        </div>
+    </HeroWrapper>
+    </>
   )
 }
 
 export default Home
+
+const HeroWrapper =  styled.div`
+background-image: url(${image});
+height: auto;
+background-repeat: no-repeat;
+background-size: cover;
+background-position: top center;
+position: relative;
+z-index: 1;
+height: 100vh;
+
+&::after{
+position: absolute;
+content: "";
+top: 0;
+left: 0;
+width: 100%;
+height: 100%;
+background-color: black;
+opacity: 0.5;
+z-index: -1;
+`
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,33 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens:{
+      // 'sm': '480px',
+      'sm' : '640px',
+      'md' : '768px',
+      'lg' : '1024px'
+    },
+    colors: {
+      blue: "#8B80B6",
+      white: '#F9FAFA',
+      brown: '#F1CF69',
+      green: '#334B35'
+    },
+    height: {
+			"10v": "10vh",
+			"20v": "20vh",
+			"30v": "30vh",
+			"40v": "40vh",
+			"50v": "50vh",
+			"60v": "60vh",
+			"70v": "70vh",
+			"80v": "80vh",
+			"90v": "90vh",
+			"100v": "100vh",
+		},
+    fontFamily: {
+      'press-start': ['"Press Start 2P"', 'Averia Serif Libre'],
+    },
     extend: {},
   },
   plugins: [],
