01cd430ca36d25f3418a37bffe041e2bb4ca1952
Marcos8060 <marcosgav80@gmail.com>
2022-08-15 18:00:44 +0300

    added stepperjs

--- src/pages/Survery.js
+++ src/pages/Survery.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { getData } from '../redux/features/data';
 import { useSelector } from 'react-redux/es/exports';
+import { Stepper,Step, StepLabel } from '@material-ui/core';
 
 const Survery = () => {
   const dispatch  = useDispatch();
@@ -18,9 +19,20 @@ const Survery = () => {
   return (
     <>
       <div className='bg-gray h-100v py-20'>
-        <div className="bg-white shadow-2xl rounded p-4 w-8/12 mx-auto p-10">
+      <Stepper className='md:w-8/12 w-11/12 mx-auto mb-10' orientation='horizontal'>
+         <Step>
+           <StepLabel></StepLabel>
+         </Step>
+         <Step>
+           <StepLabel></StepLabel>
+         </Step>
+         <Step>
+           <StepLabel></StepLabel>
+         </Step>
+       </Stepper>
+        <div className="bg-white shadow-2xl rounded p-4 md:w-8/12 w-11/12 mx-auto p-10">
           <h2 className='font-semibold mb-20'>What is your name?</h2>
-            <input name='answer1' className='p-2 focus:outline-none bg-gray w-80' type="text" placeholder='Enter your answer...' /><br /> <br />
+            <input name='answer1' className='p-2 focus:outline-none bg-gray w-7/12' type="text" placeholder='Enter your answer...' /><br /> <br />
             <button className='block float-right bg-blue px-4 py-1 font-semibold rounded text-white font-normal'>Next</button>
         </div>
       </div>
